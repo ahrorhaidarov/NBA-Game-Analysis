a477700eaac2927c04db87734d076ea9ef1d14fb
ahrorhaidarov <alanjon1312@gmail.com>
2024-02-02 22:44:48 +0500

    Project finished

---
+++ my_nba_game_analysis.py
@@ -0,0 +1,16 @@
+from functions import load_data, sort_data, calculate, conver_to_table
+
+
+def my_nba_analysis(filename):
+    global result
+
+    play = load_data(filename)
+    sort_data(play)
+
+    result = calculate(result, 'home_team')
+    result = calculate(result, 'away_team')
+
+    conver_to_table(result, 'home_team', result['home_team']['name'])
+    conver_to_table(result, 'away_team', result['away_team']['name'])
+
+my_nba_analysis('Warriors vs Thunders.txt')
